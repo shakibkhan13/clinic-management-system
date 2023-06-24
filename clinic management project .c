1b42dad93d6df06232c3d9350b5b004c534c1011
shakibkhan13 <134956975+shakibkhan13@users.noreply.github.com>
2023-06-24 17:07:48 +0600

    Update clinic management project .c

--- clinic management project .c
+++ clinic management project .c
@@ -1,41 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
 
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
+#define MAX_SPECIALTIES 100
+#define MAX_PATIENTS 100
+#define MAX_MEDICINES 100
+#define MAX_EQUIPMENT 100
 
-char num1[];
-int age[];
-int total = 0,b = 0 ;
+int count = 0;
 
-void WelcomeScreen(void);
-void Title(void);
-void MainMenu(void);
-void Add_rec(void);
-void func_List(void);
-void Search_rec(void);
-void Edit_rec(void);
-void Dlt_rec(void);
-void ex_it(void);
-
-struct patient {
+struct Speciality
+{
+    int serial_no;
+    int doc_id;
+    char name[50];
     int age;
-    char gender;
-    char first_Name[20];
-    char last_Name[20];
+    char gender[20];
+    char address[70];
     char contact_No[15];
-    char address[30];
-    char email[30];
-    char doctor[30];
-    char problem[20];
+    char symptoms[150];
+    int pre_Admission_Status;
+    int id;
+    double price;
+    float cost;
 };
-struct patient p,temp_c;
 
-int main(void) {
-    WelcomeScreen();
-    Title();
-    MainMenu();
-}
-void WelcomeScreen(void)
+struct Speciality total[MAX_SPECIALTIES];
+
+void WelcomeScreen()
 {
     printf("\n\n\n\n\n\t\t\t\t#########################################");
     printf("\n\t\t\t\t#\t\tWELCOME TO \t\t#");
@@ -45,104 +38,175 @@ void WelcomeScreen(void)
     getch();
     system("cls");
 }
-void Title(void)
+
+void Title()
 {
-        printf("\n\n\t\t-------------------------------------------------------------------------------");
-        printf("\n\t\t|                              SSA  CLINIC CENTER                             |");
-        printf("\n\t\t-------------------------------------------------------------------------------");
-}
-void MainMenu(void)
-{
-    system("cls");
-    int choose ;
-    Title();
-    printf("\n\n\n\n\n\t\t\t\t1. Add Patients Record.\n");
-    printf("\n\t\t\t\t2. Show Patients Record.\n");
-    printf("\n\t\t\t\t3. Search Patients Record.\n");
-    printf("\n\t\t\t\t4. Edit Patients Record .\n");
-    printf("\n\t\t\t\t5. Delete Patients Record .\n");
-    printf("\n\t\t\t\t6. Exit.\n");
-    printf("\n\n\n\n\t\t\t\tChoose from 1 to 6:");
-    scanf("%i",&choose);
-    switch(choose)
-    {
-        case 1 :
-            Add_rec();
-            break ;
-        case 2 :
-            Show_list();
-            break ;
-        case 3 :
-            Search_rec();
-            break ;
-        case 4 :
-            Edit_rec();
-            break ;
-        case 5 :
-            Dlt_rec();
-            break ;
-        case 6 :
-            ex_it();
-            break ;
-        default :
-            printf("\t\t\tInvalid entry . Please Enter right option :-  ");
-            getch();
-    }
+    printf("\n\n\t\t\t-------------------------------------------------------------------------------");
+    printf("\n\t\t\t|                              SSA  CLINIC CENTER                             |");
+    printf("\n\t\t\t-------------------------------------------------------------------------------");
 }
 
-void ex_it(void){
+void MainMenu()
+{
+    system("cls");
+    int choice;
+    do
+    {
+        system("cls");
+        int choose;
+        Title();
+        printf("\n\n\n\n\n\t\t\t\t\t1. Add Surgical Speciality.\n");
+        printf("\n\t\t\t\t\t2. Display Surgical Speciality.\n");
+        printf("\n\t\t\t\t\t3. Online Admission Form.\n");
+        printf("\n\t\t\t\t\t4. Display Online Admission form.\n");
+        printf("\n\t\t\t\t\t5. Add Medicine.\n");
+        printf("\n\t\t\t\t\t6. Display Medicines.\n");
+        printf("\n\t\t\t\t\t7. My Health Record.\n");
+        printf("\n\t\t\t\t\t8. Add Equipment.\n");
+        printf("\n\t\t\t\t\t9. Calculate Equipment cost.\n");
+        printf("\n\t\t\t\t\t10. Contact Us.\n");
+        printf("\n\t\t\t\t\t11. Hospital News.\n");
+        printf("\n\t\t\t\t\t12. Exit.\n");
+        printf("\n\t\t\t\t\tEnter Your choice Number(1 to 12): ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            Add_Specialty();
+            break;
+        case 2:
+            Display_Surgical();
+            break;
+        case 3:
+            On_Add_fo();
+            break;
+        case 4:
+            Display_On_Add_fo();
+            break;
+        case 5:
+            Add_medi();
+            break;
+        case 6:
+            Dis_Add_medi();
+            break;
+        case 7:
+            My_Heal_R();
+            break;
+        case 8:
+            Add_Equip();
+            break;
+        case 9:
+            cal_Equip_cost();
+            break;
+        case 10:
+            cont_us();
+            break;
+        case 11:
+            Hos_news();
+            break;
+        case 12:
+            ex_it();
+            break;
+        default:
+            printf("Invalid choice. Please try again.\n");
+            break;
+        }
+    } while (choice != 12);
+}
+
+void ex_it()
+{
     system("cls");
     Title();
-    printf("\n\n\n\n\n\n\t\t\t\t\THANK YOU FOR VISITING  :- ");
+    printf("\n\n\n\n\n\n\t\t\t\t\tTHANK YOU FOR VISITING!\n");
     getch();
 }
-void Add_rec() {
+
+void Add_Specialty()
+{
     system("cls");
-    FILE* sa;
-    sa = fopen("Record.dat", "a");
-    if (sa == NULL) {
-        printf("Error opening file.\n");
-        return;
-    }
-
-    printf("\n\n\t\t\t!!!!!!!!!!!!!!!!! Add Patients Record !!!!!!!!!!!!!!!!!");
-
-    A:
-    printf("\n\t\t\tFirst Name : ");
-    scanf("%20s", p.first_Name); // Use %20s to limit the input to 20 characters
-    p.first_Name[0] = toupper(p.first_Name[0]);
-
-    if (strlen(p.first_Name) > 20 || strlen(p.first_Name) < 2) {
-        printf("\n\t Invalid: The max range for the first name is 20 and the min range is 2.\n");
-        goto A;
-    } else {
-        int total = 1;
-        for (int b = 0; b < strlen(p.first_Name); b++) {
-            if (!isalpha(p.first_Name[b])) {
-                total = 0;
-                break;
-            }
-        }
-        if (!total) {
-            printf("\n\t\t First name contains invalid characters.\n");
-            printf("\n\t\t Please enter again: ");
-            goto A;
-        }
-    }
-
-    // Write the patient's record to the file
-    fprintf(sa, "%s\n", p.first_Name);
-
-    fclose(sa);
+    printf("\n\t\t\t---------------------------------------------------------------");
+    printf("\n\t\t\t|                          Add Specialty                      |");
+    printf("\n\t\t\t---------------------------------------------------------------");
+    struct Speciality s;
+    printf("\n\n\n\t\t\t\tEnter Your Specialty ID: ");
+    scanf("%d", &s.doc_id);
+    printf("\n\t\t\t\tEnter Your Specialty name: ");
+    scanf("%s", s.name);
+    total[count++] = s;
+    printf("\n\t\t\t\tSpecialty added successfully!\n");
+    getch();
 }
 
- void Show_list(){
+void Display_Surgical()
+{
+    system("cls");
+    Title();
+    if (count == 0)
+    {
+        printf("\n\t\t\t\tNo specialties found.\n");
+    }
+    else
+    {
+        printf("\n\t\t\t\tID\t\tSpecialties Name");
+        printf("\n\n\t\t\t\t-------------------------------");
+        for (int i = 0; i < count; i++)
+        {
+            printf("\n\t\t\t\t%d\t\t%s", total[i].doc_id, total[i].name);
+        }
+    }
+    getch();
+}
 
- }
- void Search_rec(){
- }
-  void Edit_rec(){
-  }
- void Dlt_rec(){
- }
+void On_Add_fo()
+{
 
+}
+
+void Display_On_Add_fo()
+{
+
+}
+
+void Add_medi()
+{
+
+}
+
+void Dis_Add_medi()
+{
+
+}
+
+void My_Heal_R()
+{
+
+}
+
+void Add_Equip()
+{
+
+}
+
+void cal_Equip_cost()
+{
+
+}
+
+void cont_us()
+{
+
+}
+
+void Hos_news()
+{
+
+}
+
+int main()
+{
+    WelcomeScreen();
+    Title();
+    MainMenu();
+    return 0;
+}
